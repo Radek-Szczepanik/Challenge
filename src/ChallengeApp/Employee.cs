cb4ab34ddfa6ea841b54f3773c519087c5849d82
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-11-06 20:41:54 +0100

    Refactoring

--- src/ChallengeApp/Employee.cs
+++ src/ChallengeApp/Employee.cs
@@ -33,6 +33,37 @@ namespace ChallengeApp
         public abstract Statistics GetStatistics();
     }
 
+    public class InMemoryEmployee : EmployeeBase
+    {
+        private List<double> grades;
+        public InMemoryEmployee(string name) : base(name)
+        {
+            grades = new List<double>();
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override void AddGrade(double grade)
+        {
+            if(grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+
+        public override Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
     public class SavedEmployee : EmployeeBase
     {
         public SavedEmployee(string name) : base(name)
@@ -54,7 +85,20 @@ namespace ChallengeApp
 
         public override Statistics GetStatistics()
         {
-            throw new NotImplementedException();
+            var result = new Statistics();
+
+            using(var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+
+                while(line != null)
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+            return result;
         }
     }
 }
